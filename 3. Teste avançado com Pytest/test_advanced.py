f9e5af3b1fbd84bc977f58b81e25760fc2db2a6b
Mariama <132833349+Mariamapereira@users.noreply.github.com>
2024-03-11 19:13:19 -0300

    Etapa 2 – Executar os testes

--- 3. Teste avançado com Pytest/test_advanced.py
+++ 3. Teste avançado com Pytest/test_advanced.py
@@ -51,3 +51,36 @@ class TestFile:
     #     with open("/tmp/done") as _f:
     #         contents = _f.read()
     #     assert contents == "1"
+
+
+
+#   Etapa 2 – Executar os testes e explorar o relatório:
+# Após adicionar testes, a próxima etapa é executar pytest e inspecionar a saída. Use o sinalizador de detalhamento maior (-v) para ver todos os valores de entrada tratados como um teste separado.
+
+
+Pichau@Mariama MINGW64 ~ (add-my-user-Mariamapereira)
+$ pytest -v test_code.py
+================================================================================= test session starts =================================================================================
+platform win32 -- Python 3.12.1, pytest-8.0.2, pluggy-1.4.0 -- C:\Users\Pichau\AppData\Local\Programs\Python\Python312\python.exe
+cachedir: .pytest_cache
+rootdir: C:\Users\Pichau
+collected 9 items
+
+test_code.py::test_str_to_bool_true[Y] PASSED                                                                                                                                    [ 11%]
+test_code.py::test_str_to_bool_true[y] PASSED                                                                                                                                    [ 22%] 
+test_code.py::test_str_to_bool_true[1] PASSED                                                                                                                                    [ 33%] 
+test_code.py::test_str_to_bool_true[YES] PASSED                                                                                                                                  [ 44%] 
+test_code.py::test_str_to_bool_false[N] PASSED                                                                                                                                   [ 55%]
+test_code.py::test_str_to_bool_false[n] PASSED                                                                                                                                   [ 66%] 
+test_code.py::test_str_to_bool_false[0] PASSED                                                                                                                                   [ 77%] 
+test_code.py::test_str_to_bool_false[NO] PASSED                                                                                                                                  [ 88%] 
+test_code.py::TestFile::test_file_contents PASSED                                                                                                                                [100%] 
+
+================================================================================== 9 passed in 0.03s ================================================================================== 
+
+
+
+
+
+
+
