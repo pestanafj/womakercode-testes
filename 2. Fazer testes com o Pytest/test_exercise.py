5d583f3bd4ccb7ff41ae84876876e457b918cabe
Fernanda Pestana <pestanafj@gmail.com>
2024-03-11 18:20:01 -0300

    Exercício do Curso 2 revisado

--- 2. Fazer testes com o Pytest/test_exercise.py
+++ 2. Fazer testes com o Pytest/test_exercise.py
@@ -1,10 +1,23 @@
+import pytest
+
+
 def admin_command(command, sudo=True):
+    """
+    Prefix a command with `sudo` unless it is explicitly not needed. Expects
+    `command` to be a list.
+    """
+    if not isinstance(command, list):
+        raise TypeError(
+            f"was expecting command to be a list, but got a {type(command)}"
+        )
+
     if sudo:
-        ["sudo"] + command
+        return ["sudo"] + command
     return command
 
 
 class TestAdminCommand:
+
     def command(self):
         return ["ps", "aux"]
 
@@ -16,3 +29,11 @@ class TestAdminCommand:
         result = admin_command(self.command(), sudo=True)
         expected = ["sudo"] + self.command()
         assert result == expected
+
+    def test_non_list_commands(self):
+        with pytest.raises(TypeError) as error:
+            admin_command("some command", sudo=True)
+        assert (
+            error.value.args[0]
+            == "was expecting command to be a list, but got a <class 'str'>"
+        )
